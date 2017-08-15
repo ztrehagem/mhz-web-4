c3769d4049c14d58664eadc6b2f81a42deb62650
Shigeki Suwa <suwa.shigeki@gmail.com>
2017-08-16 03:32:52 +0900

    apply from proto

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const stylus = require('gulp-stylus');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
+const plumber = require('gulp-plumber');
 const watchify = require('watchify');
 const browserify = require('browserify');
 const babelify = require('babelify');
@@ -14,10 +15,15 @@ const del = require('del');
 
 const srcPath = filepath => path.resolve(__dirname, 'src', filepath);
 const destPath = filepath => path.resolve(__dirname, 'assets', filepath);
-const SRC = {
+const ENTRIES = {
   PUG: srcPath('views/index.pug'),
   STYLUS: srcPath('styles/master.styl'),
   JS: srcPath('scripts/app.js'),
+};
+const WATCH = {
+  PUG: ENTRIES.PUG,
+  STYLUS: srcPath('styles/**/*.styl'),
+  JS: srcPath('scripts/**/*.js'),
 };
 const DEST = {
   HTML: destPath(''),
@@ -28,16 +34,19 @@ const flags = {
   watchingJs: false,
 };
 
-del.sync([destPath('!(.git)')]);
+// del.sync([destPath('!(.git)'), destPath('!(img)')]);
+del.sync([destPath('!(.git|favicon.ico|img/)')])
 
 gulp.task('enable-wathing-js', () => flags.watchingJs = true);
 
-gulp.task('pug', () => gulp.src(SRC.PUG)
+gulp.task('pug', () => gulp.src(ENTRIES.PUG)
+  .pipe(plumber())
   .pipe(pug())
   .pipe(gulp.dest(DEST.HTML))
 );
 
-gulp.task('stylus', () => gulp.src(SRC.STYLUS)
+gulp.task('stylus', () => gulp.src(ENTRIES.STYLUS)
+  .pipe(plumber())
   .pipe(stylus({compress: true}))
   .pipe(gulp.dest(DEST.CSS))
 );
@@ -46,7 +55,7 @@ gulp.task('js', () => {
   const destFileName = 'app.js';
 
   const bundler = browserify({
-    entries: [SRC.JS],
+    entries: ENTRIES.JS,
     debug: true,
     plugin: flags.watchingJs ? watchify : null,
   }).transform(babelify, {presets: 'es2015'});
@@ -54,6 +63,7 @@ gulp.task('js', () => {
   const bundle = () => bundler.bundle()
     .on('error', () => console.log('bundle error'))
     .pipe(source(destFileName))
+    .pipe(plumber())
     .pipe(buffer())
     .pipe(uglify())
     .pipe(gulp.dest(DEST.JS))
@@ -68,6 +78,6 @@ gulp.task('default', ['pug', 'stylus', 'js']);
 
 gulp.task('w', ['watch']);
 gulp.task('watch', ['enable-wathing-js', 'default'], () => {
-  gulp.watch(SRC.PUG, ['pug']);
-  gulp.watch(SRC.STYLUS, ['stylus']);
+  gulp.watch(WATCH.PUG, ['pug']);
+  gulp.watch(WATCH.STYLUS, ['stylus']);
 });
